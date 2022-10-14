4445e475b90dae63d083e857f7165d16b683208a
teys1s <oleksii2109@ukr.net>
2022-10-14 11:52:49 +0200

    upd tests

--- pushsdkandroid/src/androidTest/java/PushSDKAndroidTest.kt
+++ pushsdkandroid/src/androidTest/java/PushSDKAndroidTest.kt
@@ -1,8 +1,14 @@
 import android.content.Context
+import android.util.Log
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.platform.app.InstrumentationRegistry
+import com.google.firebase.messaging.RemoteMessage
+import com.google.gson.Gson
 import com.push.android.pushsdkandroid.PushSDK
 import com.push.android.pushsdkandroid.core.APIHandler
+import com.push.android.pushsdkandroid.managers.PushSdkNotificationManager
+import com.push.android.pushsdkandroid.models.PushDataMessageImageModel
+import com.push.android.pushsdkandroid.models.PushDataMessageModel
 import com.push.android.pushsdkandroid.utils.Info
 import junit.framework.TestCase
 import org.junit.Before
@@ -15,13 +21,76 @@ class PushSDKAndroidTest {
     private lateinit var appContext: Context
     private lateinit var pushSDK: PushSDK
     private lateinit var apiHandler: APIHandler
-
+    private lateinit var notificationManager: PushSdkNotificationManager
+    private lateinit var message: String
+    private lateinit var remoteMessage: RemoteMessage
+    private val map: HashMap<String, String> = HashMap()
 
     @Before
     fun before() {
         appContext = InstrumentationRegistry.getInstrumentation().targetContext
         pushSDK = PushSDK(appContext, "https://example.io/api/3.0/")
         apiHandler = APIHandler(appContext)
+        notificationManager = PushSdkNotificationManager(appContext, Pair("source", "message"))
+
+        message = "{\"button\":{},\"image\":{\"url\":\"https://test_image.jpg\"},\"partner\":\"push\",\"phone\":\"0123456789\",\"messageId\":\"6c000000-4b8f-11ed-972a-0000006010000\",\"time\":\"2022-06-22T07:34:53.738326+00\",\"body\":\"Test Message Android\",\"title\":\"Test title Android\"}"
+        map["source"] = "Messaging HUB"
+        map["message"] = message.toString()
+        remoteMessage = RemoteMessage.Builder("internal").setData(map).build()
+    }
+
+    @Test
+    @Throws(Exception::class)
+    fun testConstructNotification() {
+       val construct1 = notificationManager.constructNotification(
+            remoteMessage.data,
+            PushSdkNotificationManager.NotificationStyle.NO_STYLE
+        )
+
+        val construct2 = notificationManager.constructNotification(
+            remoteMessage.data,
+            PushSdkNotificationManager.NotificationStyle.BIG_TEXT
+        )
+
+        val construct3 = notificationManager.constructNotification(
+            remoteMessage.data,
+            PushSdkNotificationManager.NotificationStyle.BIG_PICTURE
+        )
+        println(construct1)
+        println(construct2)
+        println(construct3)
+
+        val isSent = construct2?.let { notificationManager.sendNotification(it) }
+
+        println(isSent)
+    }
+
+    @Test
+    @Throws(Exception::class)
+    fun testIsChannelMuted() {
+        val result = notificationManager.isNotificationChannelMuted(PushSdkNotificationManager.DEFAULT_NOTIFICATION_CHANNEL_ID)
+        println(result)
+    }
+
+    @Test
+    @Throws(Exception::class)
+    fun testDisturbModeActive() {
+        val result = notificationManager.isDoNotDisturbModeActive()
+        println(result)
+    }
+
+    @Test
+    @Throws(Exception::class)
+    fun testNotificationsEnabled() {
+        val result = notificationManager.areNotificationsEnabled()
+        println(result)
+    }
+
+    @Test
+    @Throws(Exception::class)
+    fun testSpaceForNotification() {
+        val result = notificationManager.hasSpaceForNotification(false)
+        println(result)
     }
 
     @Test
@@ -188,3 +257,15 @@ class PushSDKAndroidTest {
         println(Info.getPhoneType(appContext))
     }
 }
+
+
+internal data class MessageModel(
+    val messageId: String,
+    val title: String,
+    val body: String,
+    val image: MessageImageModel,
+)
+
+internal data class MessageImageModel(
+    val url: String
+)
